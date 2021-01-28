23de9f44a2474cc82bfa6cd723b937439a936a75
pro-30 <37265520+pro-30@users.noreply.github.com>
2021-01-29 01:04:30 +0530

    Update gcd-and-lcm.c

--- gcd-and-lcm.c
+++ gcd-and-lcm.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-/*int aman(low,high){
-	int i=2,j=1;
-	while(low>=i){
-		if(low%i==0 && high%i==0){
-			j*=i;
-			low/=i;
-			i--;
-		}
-		i++;
-	}
-	return j;
-}*/
-
 int main(){
 	int a;
 	scanf("%d",&a);
